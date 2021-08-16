5ebddd20be5e7b6e33666a5197f19c1f57a67acf
Himanshu Patel <himanshu.patel_cs19@gla.ac.in>
2021-08-16 12:29:22 +0530

    created shortcut for users list on dashboard

--- src/pages/AppContainer/Dashboard.page.tsx
+++ src/pages/AppContainer/Dashboard.page.tsx
@@ -30,6 +30,18 @@ const Dashboard: FC<Props> = () => {
             </h1>
           </Link>
         </div>
+        <div className="bg-white w-96 sm:ml-2 h-20 md:ml-24 ml-28 rounded-2xl mt-3 shadow-lg-success-50">
+          <Link to="/users" className="cursor-pointer">
+            <h1 className="text-xl pt-6 tracking-wider text-green-500 animate-bounce">
+              <img
+                src="https://image.flaticon.com/icons/png/512/1059/1059208.png"
+                alt=""
+                className="w-8 mr-2 h-8 inline"
+              />
+              Click Here To User's List
+            </h1>
+          </Link>
+        </div>
       </div>
     </div>
   );
