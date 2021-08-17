141aff06507f8d9607b49307c2e7f9c214ffde03
Himanshu Patel <himanshu.patel_cs19@gla.ac.in>
2021-08-17 18:48:39 +0530

    removed ch for drop down with no options

--- src/components/SideBar/SideBarSingle.tsx
+++ src/components/SideBar/SideBarSingle.tsx
@@ -17,7 +17,6 @@ const SideBarSingle: FC<Props> = ({ lead, icon }) => {
           <Menu.Button className="relative flex  py-3 hover:bg-white rounded-md  cursor-pointer px-3">
             <div className="w-6 h-6 text-green-600 ml-2">{icon}</div>
             <h1>{lead}</h1>
-            <BsChevronDown className=" my-auto text-black mx-3" />
           </Menu.Button>
         </div>
       </Menu>
